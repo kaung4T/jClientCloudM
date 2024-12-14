b0a86301c6b21ca3d3e0c560c2ccea29235ff994
kaung4T <mrkaungminnkhant@gmail.com>
2024-12-15 01:24:29 +0630

    update

--- app/infoApi/page.tsx
+++ app/infoApi/page.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
 function InfoApi() {
+
+  
   return (
    <>
-        <div className={'context'}>
-            InfoApi
+        <div className="context tracking-widest">
+          <div className="mb-5">
+            <p className="text-center mb-14 text-lg">API Documentation</p>
+            <p>/api/auth/create_token</p>
+            <a href="auth.png" target="_blank">
+            <img src="auth.png" className="object-cover w-100 h-100" alt="" />
+            </a>
+
+            <p className="mt-1">/api/auth/create_token</p>
+            <p className="mb-1">/api/auth/refresh_token</p>
+            <p className="mb-1">/api/auth/logout_token</p>
+          </div>
+          
+          <div className="pt-5 border-t">
+            <p className="mb-1">/api/create_user</p>
+            <p className="mb-1">/api/all_item</p>
+            <p className="mb-1">/api/create_item</p>
+            <p className="mb-1">/api/update_item/id</p>
+            <p className="mb-1">/api/delete_item/id</p>
+          </div>
+
         </div>
    </>
   )
